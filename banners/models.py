11bcf39fe4758cc55efc966e429b34c6aa686939
Joshua <joshuanazareth97@gmail.com>
2018-10-22 03:53:21 +0530

    Added model for booking period

--- banners/models.py
+++ banners/models.py
@@ -2,3 +2,9 @@ from django.db import models
 
 class Banner(models.Model):
     name = models.CharField(max_length=200)
+
+class BookingPeriod(models.Model):
+    start_date = models DateTimeField('Booking Start Date')
+    end_date = models DateTimeField('Booking End Date')
+    banner = models.ForeignKey(Banner, on_delete=models.CASCADE)
+    

e993f0936819a1bbcaec0ea690a43f0b0610f701
Joshua <joshuanazareth97@gmail.com>
2018-10-22 03:57:58 +0530

    Fixed error in model field definition

--- banners/models.py
+++ banners/models.py
@@ -4,7 +4,12 @@ class Banner(models.Model):
     name = models.CharField(max_length=200)
 
 class BookingPeriod(models.Model):
-    start_date = models DateTimeField('Booking Start Date')
-    end_date = models DateTimeField('Booking End Date')
+    start_date = models.DateTimeField('Booking Start Date')
+    end_date = models.DateTimeField('Booking End Date')
     banner = models.ForeignKey(Banner, on_delete=models.CASCADE)
-    
+
+class PricePeriod(models.Model):
+    start_date = models.DateTimeField('Booking Start Date')
+    end_date = models.DateTimeField('Booking End Date')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    banner = models.ForeignKey(Banner, on_delete=models.CASCADE)
